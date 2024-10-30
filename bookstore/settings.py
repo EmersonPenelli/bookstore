12c729925d9ff9c86f92a40d6af07d496c87c7aa
EmersonPenelli <emersonpenelli@gmail.com>
2024-10-30 20:10:38 -0300

    fix settings and WSGI

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "9oo4ilkuwgg4f1%0ivt0!@^l7s&=npdh30@a%_g@*z9#!k4$v3"
+SECRET_KEY = "django-insecure-k=5)efvp)#n*wdhrr*utx7p&(c__@yxzj%#p2x=kws*s3a$hue"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
